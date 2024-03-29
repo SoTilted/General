f9baaedbfa6bd5f36f132d8966820dfb48868252
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-03-29 20:19:26 +0200

    Made a drop-down menu

--- js/Drop-down-menu/src/index.js
+++ js/Drop-down-menu/src/index.js
@@ -1,1 +1,18 @@
-'import style.css'
+import './style.css';
+
+const dropDownMenuButton = document.querySelector('.navbar');
+const dropDownMenu = document.querySelector('.menu');
+
+dropDownMenuButton.addEventListener('click',toggleMenu);
+
+function toggleMenu () {
+    if (dropDownMenu.style.visibility === 'hidden'){
+        dropDownMenu.style.visibility = 'visible';
+        dropDownMenu.style.opacity = '1';
+        dropDownMenu.style.transform = 'translateY(0)';
+
+    } else {
+        dropDownMenu.style.visibility='hidden'
+        dropDownMenu.style.opacity = '0';
+    }
+};
